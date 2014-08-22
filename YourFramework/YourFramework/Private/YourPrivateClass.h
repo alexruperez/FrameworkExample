3764d0ad585160aeb851de0d23887a8258971624
Alejandro Rupérez <alexruperez@gmail.com>
2014-08-22 10:51:54 +0200

    Added YourPrivateClass

---
+++ YourFramework/YourFramework/Private/YourPrivateClass.h
@@ -0,0 +1,16 @@
+//
+//  YourPrivateClass.h
+//  YourFramework
+//
+//  Created by Alejandro Rupérez on 22/08/14.
+//  Copyright (c) 2014 Gigigo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/// This is your private class.
+@interface YourPrivateClass : NSObject
+
++ (BOOL)isYourFrameworkWorking;
+
+@end
--- YourFramework/YourFramework/Private/YourPrivateClass.m
+++ YourFramework/YourFramework/Private/YourPrivateClass.m
@@ -1,20 +1,19 @@
 //
-//  YourClass.m
+//  YourPrivateClass.m
 //  YourFramework
 //
-//  Created by Alejandro Rupérez on 21/08/14.
+//  Created by Alejandro Rupérez on 22/08/14.
 //  Copyright (c) 2014 Gigigo. All rights reserved.
 //
 
-#import "YourClass.h"
 #import "YourPrivateClass.h"
 
-@implementation YourClass
+@implementation YourPrivateClass
 
 /// Is your framework working?
 + (BOOL)isYourFrameworkWorking
 {
-    return YourPrivateClass.isYourFrameworkWorking;
+    return YES;
 }
 
 @end
